c3665c2d9add7ebe5bdd3798ded08021b5d509ed
khandelwalriya <rkhandelwal2793@gmail.com>
2016-04-02 19:15:06 +0530

    new code check in for todo list

--- todolist/script.js
+++ todolist/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $('#button').click(function(){
         var toAdd= $('input[name=checkListItem]').val();
         $('.list').append("<div class='item'>"+toAdd+"</div>");
@@ -6,4 +6,40 @@ $(document).ready(function(){
     $(document).on('click','.item',function(){
         $(this).remove();
         });
+    }); */
+var template = function(text) {
+  return '<p><input type="checkbox"><i class="glyphicon glyphicon-star"></i><span>' + text + '</span><i class="glyphicon glyphicon-remove"></i></p>';
+};
+var add=function(item){
+ var html=template(item);
+    $(html).appendTo('.list');
+};
+
+var main = function() {
+  $('form').submit(function() {
+     var text= $('#todo').val();
+    if(text!=="")
+    {
+   var html=template(text);
+    $(html).appendTo('.list');
+    $('#todo').val("");
+    }
+   
+    return false;  
+  });
+  
+   $(document).on("click",'.glyphicon-remove',function(){
+   $(this).parent().remove();
+   });
+  
+  $(document).on("click",'.glyphicon-star',function(){
+    $(this).toggleClass('active');
     });
+  if(annyang){
+  var commands={'add *item' : add, }
+annyang.addCommands(commands);
+annyang.start();
+}
+}; 
+
+$(document).ready(main);
